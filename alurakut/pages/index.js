825a3e2d9e3e833d7495656741d5138cbb3b601f
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-07-17 18:40:24 -0300

    Iniciando - Aula 02 - React, State e o primeiro formulário

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,12 +1,21 @@
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades){
   return (
     <Box>
       <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }}/>
+      <hr />
+      <p>
+        <a className="boxLink" href={`hhtps://github.com/${propriedades.githubUser}`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
@@ -38,6 +47,34 @@ export default function Home() {
           </h1>
 
           <OrkutNostalgicIconSet />
+        </Box>
+
+        <Box>
+          <h2 className="subTitle">O que você deseja fazer?</h2>
+          <form onSubmit={function handleCriaComunidade(e){
+            e.preventDefault();
+          }}>
+            <div>
+              <input 
+                placeholder="Qual vai ser o nome da sua comunidade?" 
+                name="title" 
+                aria-label="Qual vai ser o nome da sua comunidade?"
+                type="text"
+              />
+            </div>
+
+            <div>
+              <input 
+                placeholder="Coloque uma URL para usarmos de capa" 
+                name="image" 
+                aria-label="Coloque uma URL para usarmos de capa"
+              />
+            </div>
+
+            <button>
+              Criar comunidade
+            </button>
+          </form>
         </Box>
       </div>
       
