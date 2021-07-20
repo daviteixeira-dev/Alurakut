6b8348e7bb29aa19cf519f86242e971140c5200d
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-07-20 00:30:37 -0300

    Finalização - Aula 05 - Imersão React

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -43,9 +45,9 @@ function ProfileRelationsBox(propriedades){
   )
 }
 
-export default function Home() {
+export default function Home(props) {
 
-  const usuarioAleatorio = 'daviteixeira-btm';
+  const usuarioAleatorio = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'juunegreiros', 
@@ -204,4 +206,35 @@ export default function Home() {
     </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context){
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  console.log('isAuthenticated', isAuthenticated);
+
+  if(!isAuthenticated){
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
